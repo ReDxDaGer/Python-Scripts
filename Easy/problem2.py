ebbd3b3c71cc3ab432332544ba035bee31118705
ReDxDaGer <yashmehrotra45@gmail.com>
2024-03-02 19:14:11 +0530

    Added the easy problem 2

---
+++ Easy/problem2.py
@@ -0,0 +1,8 @@
+gender = input("provide your gender as F or M")
+
+if gender == "M" or gender == "m":
+    print("Good morning Sir")
+elif gender == "F" or gender == "f":
+    print("Good morning Mam")
+else:
+    print("Wrong input")
