1023db2000032005838ba80b826b3e1a14edb3a8
ReDxDaGer <yashmehrotra45@gmail.com>
2024-03-02 19:13:05 +0530

    u

---
+++ Easy/problem1.py
@@ -0,0 +1,9 @@
+a = int(input("PLEASE GIVE YOUR FIRST NUMBER"))
+b = int(input("PLEASE PROVIDE YOUR 2ND NUMBER"))
+
+if a > b:
+     print(f"{a} is greater than {b}")
+elif b > a:
+     print(f"{b} is greater than {a}")
+else:
+     print("both the numbers are equal")
